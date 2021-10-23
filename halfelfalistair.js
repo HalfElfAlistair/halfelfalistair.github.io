9a8c64fc71b9317934fbc1a4a62d52a9a8e5d1c2
HalfElfAlistair <ajss90@hotmail.co.uk>
2021-10-23 08:55:00 +0100

    made some minor amendments to js

--- halfelfalistair.js
+++ halfelfalistair.js
@@ -1,12 +1,13 @@
 function hamburgerMenu() {
 
     // Assigns the navbar element as a variable
-    const x = document.getElementById("menu");
+    const menu = document.getElementById("menu");
 
     // When the nav class is in it's standard state (assigned as "navbar" in HTML), it concatenates the string " switch" to add an alternate class. This new class is used in CSS to change the appearance of the page when the button is pressed. It reverts back to it's original class name when the button is pressed and the name already possessed the " switch" concatenation.
-    if (x.className === "navbar") {
-        x.className += " switch";
+    if (menu.className === "navbar") {
+        menu.className += " extend";
+        // menu.className += " switch";
     } else {
-        x.className = "navbar";
+        menu.className = "navbar";
     }
     }
