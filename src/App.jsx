4b98f90d59d74398239111a7553c010fa4a35172
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2025-08-28 11:58:42 +0100

    adding content to App file, mostly static at present

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,110 @@
+import { Nav } from './components/Nav';
+import './app.css';
+import aliPhotoCrop from './images/aliPhotoCrop.jpg';
+import caramelShortbread from './images/caramelShortbread.jpg';
+import ncNewsBackendScreeny from './images/ncNewsBackendScreeny.png';
+import ncNewsFrontendScreeny from './images/ncNewsFrontendScreeny.png';
+
 function App() {
 
     return (
         <>
-            Alistair's App
+            <Nav />
+            <header>
+                <div id="intro">
+                    <h1>Introducing: Alistair Sinclair-Smith</h1>
+                    <p>A convert from SEO to Full Stack Development.</p>
+                </div>
+                <div id="top-img">
+                    <img src={aliPhotoCrop} alt="Alistair Sinclair-Smith hanging out at a friend's flat" />
+                </div>
+            </header>
+            <main>
+                <section id="about">
+                    <div className="content">
+                        <h2>About Me</h2>
+                        <div id="professional">
+                            <h3>Professional</h3>
+
+                            <p>I'm Alistair-Sinclair-Smith, a 2011 Marketing graduate who's since tried his hand in various fields; from customer service to data admin.<br /><br /> After a number of years working with on-page <abbr title="Search Engine Optimisation">SEO</abbr>, I found myself exploring coding as a new skill.<br /><br />Initially drawn by the user experience and problem-solving aspects, it dawned on me that this was something I enjoyed and wanted to devote myself to full-time.</p>
+
+                            <p>Basic online courses, like <a href="https://www.freecodecamp.org/" target="_blank" rel="noopener">freeCodeCamp</a>, for JavaScript, HTML and CSS gave me a first taste. I also explored SCSS, jQuery and Bootstrap, with personal projects, but felt I needed something else to take me to the next step.<br /><br /> I decided to enrol on the bootcamp at <a href="https://northcoders.com/" target="_blank" rel="noopener">Northcoders</a>, seeking a knowledge of industry best practice and an upgrade to my skillset.</p>
+
+                            <p>While improving my knowledge of the fundamentals, the course enabled me to use React, PSQL, APIs, Test Driven Development, and agile working practices. Additionally, coding full time, at the bootcamp, has prepared me for a career in development and reinforced my desire to be able to do that every day.</p>
+
+                        </div>
+                        <div id="personal">
+                            <h3>Personal</h3>
+                            <p>I'm an empathetic person and am often pondering how to make life better for people, hence the aforementioned UX draw to coding. I'd love to someday be in a position to have a greater positive influence on the world around me.
+                                <br /><br /> Of course, it would also be brilliant to produce code that benefits users, for an organisation that is helping make the world a better place. So finding that kind of role is certainly something to aspire to.
+                                <br /><br />Aside from coding, my spare time is devoted to a mix of hobbies which include football, gaming, Dungeons & Dragons, and baking (see picture below).
+                                <br /><br />If you check out some of my repositories on GitHub you'll find I've used D&D as an influence for some of my projects.</p>
+                            <img src={caramelShortbread} alt="Caramel Shortbread made by Alistair Sinclair-Smith" />
+                        </div>
+                    </div>
+                </section>
+                <section id="knowledge">
+                    <div className="content">
+                        <h2>My Knowledge</h2>
+                        <p>Some of the useful knowledge and skills I possess.</p>
+                        <div className="container">
+                            <div className="KST" id="knowledge_box_1">
+                                <ul>
+                                    <li>HTML5</li>
+                                    <li>CSS3</li>
+                                    <li>JavaScript</li>
+                                </ul>
+                            </div>
+                            <div className="KST" id="knowledge_box_2">
+                                <ul>
+                                    <li>SQL</li>
+                                    <li>React</li>
+                                    <li>Firebase</li>
+                                </ul>
+                            </div>
+                            <div className="KST" id="knowledge_box_3">
+                                <ul>
+                                    <li>Responsive Design</li>
+                                    <li>On-page SEO</li>
+                                    <li>Test Driven Development</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section id="projects">
+                    <div className="content">
+                        <h2>My Projects</h2>
+                        <p>Please feel free to check out <a href="https://github.com/HalfElfAlistair" target="_blank" rel="noopener">my GitHub account</a> where you'll find various repositories, both complete and some works in progress. They'll give you a better idea of where I'm at and how far I've come. Below you'll see a small selection from those repos, which link to their respective location.</p>
+                        <div className="container">
+                            <div className="project-box" id="LP">
+                                <a href="https://github.com/HalfElfAlistair/nc-news.git" target="_blank" rel="noopener" className="project-tile">
+                                    <p>Frontend Project: NC-News</p>
+                                    <img src={ncNewsFrontendScreeny} alt="A screenshot from my Northcoders Frontend project." target="_blank" rel="noopener" />
+                                </a>
+                            </div>
+                            <div className="project-box" id="SF">
+                                <a href="https://github.com/HalfElfAlistair/nc_backend_project.git" target="_blank" rel="noopener" className="project-tile">
+                                    <p>Backend Project: NC-News API</p>
+                                    <img src={ncNewsBackendScreeny} alt="A screenshot from my Northcoders Backend project." target="_blank" rel="noopener" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+            <footer id="contact">
+                <div className="content">
+                    <h2>Get in touch with Alistair Sinclair-Smith</h2>
+                    <p>Drop me an email and let's discuss how I can help you!</p>
+                    <p>Email: <a href="mailto:alistairjss90@gmail.com" target="_blank" rel="noopener">alistairjss90@gmail.com</a></p>
+                    <p>Or check me out on social media.</p>
+                    <div id="icons">
+                        <a href="https://uk.linkedin.com/in/alistair-sinclair-smith-b247884a" aria-label="Icon link for Alistair Sinclair-Smith's LinkedIn profile." target="_blank" rel="noopener" className="nav-link"><svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="linkedin" className="svg-inline--fa fa-linkedin fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"></path></svg></a>
+                        <a href="https://github.com/HalfElfAlistair" aria-label="Icon link for Alistair Sinclair-Smith's GitHub profile." target="_blank" rel="noopener" className="nav-link"><svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github-square" className="svg-inline--fa fa-github-square fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM277.3 415.7c-8.4 1.5-11.5-3.7-11.5-8 0-5.4.2-33 .2-55.3 0-15.6-5.2-25.5-11.3-30.7 37-4.1 76-9.2 76-73.1 0-18.2-6.5-27.3-17.1-39 1.7-4.3 7.4-22-1.7-45-13.9-4.3-45.7 17.9-45.7 17.9-13.2-3.7-27.5-5.6-41.6-5.6-14.1 0-28.4 1.9-41.6 5.6 0 0-31.8-22.2-45.7-17.9-9.1 22.9-3.5 40.6-1.7 45-10.6 11.7-15.6 20.8-15.6 39 0 63.6 37.3 69 74.3 73.1-4.8 4.3-9.1 11.7-10.6 22.3-9.5 4.3-33.8 11.7-48.3-13.9-9.1-15.8-25.5-17.1-25.5-17.1-16.2-.2-1.1 10.2-1.1 10.2 10.8 5 18.4 24.2 18.4 24.2 9.7 29.7 56.1 19.7 56.1 19.7 0 13.9.2 36.5.2 40.6 0 4.3-3 9.5-11.5 8-66-22.1-112.2-84.9-112.2-158.3 0-91.8 70.2-161.5 162-161.5S388 165.6 388 257.4c.1 73.4-44.7 136.3-110.7 158.3zm-98.1-61.1c-1.9.4-3.7-.4-3.9-1.7-.2-1.5 1.1-2.8 3-3.2 1.9-.2 3.7.6 3.9 1.9.3 1.3-1 2.6-3 3zm-9.5-.9c0 1.3-1.5 2.4-3.5 2.4-2.2.2-3.7-.9-3.7-2.4 0-1.3 1.5-2.4 3.5-2.4 1.9-.2 3.7.9 3.7 2.4zm-13.7-1.1c-.4 1.3-2.4 1.9-4.1 1.3-1.9-.4-3.2-1.9-2.8-3.2.4-1.3 2.4-1.9 4.1-1.5 2 .6 3.3 2.1 2.8 3.4zm-12.3-5.4c-.9 1.1-2.8.9-4.3-.6-1.5-1.3-1.9-3.2-.9-4.1.9-1.1 2.8-.9 4.3.6 1.3 1.3 1.8 3.3.9 4.1zm-9.1-9.1c-.9.6-2.6 0-3.7-1.5s-1.1-3.2 0-3.9c1.1-.9 2.8-.2 3.7 1.3 1.1 1.5 1.1 3.3 0 4.1zm-6.5-9.7c-.9.9-2.4.4-3.5-.6-1.1-1.3-1.3-2.8-.4-3.5.9-.9 2.4-.4 3.5.6 1.1 1.3 1.3 2.8.4 3.5zm-6.7-7.4c-.4.9-1.7 1.1-2.8.4-1.3-.6-1.9-1.7-1.5-2.6.4-.6 1.5-.9 2.8-.4 1.3.7 1.9 1.8 1.5 2.6z"></path></svg></a>
+                    </div>
+                </div>
+            </footer>
         </>
     )
 }
