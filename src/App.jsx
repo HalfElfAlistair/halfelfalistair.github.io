cb17dd74c44606e6705ea8bdc10cf003b37cb034
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2025-08-31 23:45:18 +0100

    replacing static code with new components and icons

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,50 @@ import aliPhotoCrop from './images/aliPhotoCrop.jpg';
 import caramelShortbread from './images/caramelShortbread.jpg';
 import ncNewsBackendScreeny from './images/ncNewsBackendScreeny.png';
 import ncNewsFrontendScreeny from './images/ncNewsFrontendScreeny.png';
+import { KnowledgeCard } from './components/KnowledgeCard';
+import { ProjectCard } from './components/ProjectCard';
+import { LinkedIn } from './icons/LinkedIn';
+import { GitHub } from './icons/GitHub';
 
 function App() {
+
+    const knowledgeList = [
+        ['HTML5', 'CSS3', 'JavaScript'],
+        ['SQL', 'React', 'Firebase'],
+        ['Responsive Design', 'On-page SEO', 'Test Driven Development']
+    ]
+
+    const projects = [
+        {
+            id: 'LP',
+            path: 'nc-news',
+            title: 'Frontend Project: NC-News',
+            image: ncNewsFrontendScreeny,
+            imageAlt: 'A screenshot from my Northcoders Frontend project.'
+        },
+        {
+            id: 'SF',
+            path: 'nc_backend_project',
+            title: 'Backend Project: NC-News API',
+            image: ncNewsBackendScreeny,
+            imageAlt: 'A screenshot from my Northcoders Backend project.'
+        }
+    ]
+
+    const fillColour = '#004C54';
+
+    const socials = [
+        {
+            path: 'https://uk.linkedin.com/in/alistair-sinclair-smith-b247884a',
+            label: "Icon link for Alistair Sinclair-Smith's LinkedIn profile.",
+            icon: <LinkedIn fillColour={fillColour} />
+        },
+        {
+            path: 'https://github.com/HalfElfAlistair',
+            label: "Icon link for Alistair Sinclair-Smith's GitHub profile.",
+            icon: <GitHub fillColour={fillColour} />
+        }
+    ]
 
     return (
         <>
@@ -48,27 +90,9 @@ function App() {
                         <h2>My Knowledge</h2>
                         <p>Some of the useful knowledge and skills I possess.</p>
                         <div className="container">
-                            <div className="KST" id="knowledge_box_1">
-                                <ul>
-                                    <li>HTML5</li>
-                                    <li>CSS3</li>
-                                    <li>JavaScript</li>
-                                </ul>
-                            </div>
-                            <div className="KST" id="knowledge_box_2">
-                                <ul>
-                                    <li>SQL</li>
-                                    <li>React</li>
-                                    <li>Firebase</li>
-                                </ul>
-                            </div>
-                            <div className="KST" id="knowledge_box_3">
-                                <ul>
-                                    <li>Responsive Design</li>
-                                    <li>On-page SEO</li>
-                                    <li>Test Driven Development</li>
-                                </ul>
-                            </div>
+                            {knowledgeList.map((items, i) => {
+                                return <KnowledgeCard key={`box${i}`} boxNumber={i + 1} items={items} />
+                            })}
                         </div>
                     </div>
                 </section>
@@ -77,18 +101,10 @@ function App() {
                         <h2>My Projects</h2>
                         <p>Please feel free to check out <a href="https://github.com/HalfElfAlistair" target="_blank" rel="noopener">my GitHub account</a> where you'll find various repositories, both complete and some works in progress. They'll give you a better idea of where I'm at and how far I've come. Below you'll see a small selection from those repos, which link to their respective location.</p>
                         <div className="container">
-                            <div className="project-box" id="LP">
-                                <a href="https://github.com/HalfElfAlistair/nc-news.git" target="_blank" rel="noopener" className="project-tile">
-                                    <p>Frontend Project: NC-News</p>
-                                    <img src={ncNewsFrontendScreeny} alt="A screenshot from my Northcoders Frontend project." target="_blank" rel="noopener" />
-                                </a>
-                            </div>
-                            <div className="project-box" id="SF">
-                                <a href="https://github.com/HalfElfAlistair/nc_backend_project.git" target="_blank" rel="noopener" className="project-tile">
-                                    <p>Backend Project: NC-News API</p>
-                                    <img src={ncNewsBackendScreeny} alt="A screenshot from my Northcoders Backend project." target="_blank" rel="noopener" />
-                                </a>
-                            </div>
+                            {projects.map(project => {
+                                const { id, path, title, image, imageAlt } = project;
+                                return <ProjectCard key={id} id={id} path={path} title={title} image={image} imageAlt={imageAlt} />
+                            })}
                         </div>
                     </div>
                 </section>
@@ -100,8 +116,14 @@ function App() {
                     <p>Email: <a href="mailto:alistairjss90@gmail.com" target="_blank" rel="noopener">alistairjss90@gmail.com</a></p>
                     <p>Or check me out on social media.</p>
                     <div id="icons">
-                        <a href="https://uk.linkedin.com/in/alistair-sinclair-smith-b247884a" aria-label="Icon link for Alistair Sinclair-Smith's LinkedIn profile." target="_blank" rel="noopener" className="nav-link"><svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="linkedin" className="svg-inline--fa fa-linkedin fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"></path></svg></a>
-                        <a href="https://github.com/HalfElfAlistair" aria-label="Icon link for Alistair Sinclair-Smith's GitHub profile." target="_blank" rel="noopener" className="nav-link"><svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github-square" className="svg-inline--fa fa-github-square fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM277.3 415.7c-8.4 1.5-11.5-3.7-11.5-8 0-5.4.2-33 .2-55.3 0-15.6-5.2-25.5-11.3-30.7 37-4.1 76-9.2 76-73.1 0-18.2-6.5-27.3-17.1-39 1.7-4.3 7.4-22-1.7-45-13.9-4.3-45.7 17.9-45.7 17.9-13.2-3.7-27.5-5.6-41.6-5.6-14.1 0-28.4 1.9-41.6 5.6 0 0-31.8-22.2-45.7-17.9-9.1 22.9-3.5 40.6-1.7 45-10.6 11.7-15.6 20.8-15.6 39 0 63.6 37.3 69 74.3 73.1-4.8 4.3-9.1 11.7-10.6 22.3-9.5 4.3-33.8 11.7-48.3-13.9-9.1-15.8-25.5-17.1-25.5-17.1-16.2-.2-1.1 10.2-1.1 10.2 10.8 5 18.4 24.2 18.4 24.2 9.7 29.7 56.1 19.7 56.1 19.7 0 13.9.2 36.5.2 40.6 0 4.3-3 9.5-11.5 8-66-22.1-112.2-84.9-112.2-158.3 0-91.8 70.2-161.5 162-161.5S388 165.6 388 257.4c.1 73.4-44.7 136.3-110.7 158.3zm-98.1-61.1c-1.9.4-3.7-.4-3.9-1.7-.2-1.5 1.1-2.8 3-3.2 1.9-.2 3.7.6 3.9 1.9.3 1.3-1 2.6-3 3zm-9.5-.9c0 1.3-1.5 2.4-3.5 2.4-2.2.2-3.7-.9-3.7-2.4 0-1.3 1.5-2.4 3.5-2.4 1.9-.2 3.7.9 3.7 2.4zm-13.7-1.1c-.4 1.3-2.4 1.9-4.1 1.3-1.9-.4-3.2-1.9-2.8-3.2.4-1.3 2.4-1.9 4.1-1.5 2 .6 3.3 2.1 2.8 3.4zm-12.3-5.4c-.9 1.1-2.8.9-4.3-.6-1.5-1.3-1.9-3.2-.9-4.1.9-1.1 2.8-.9 4.3.6 1.3 1.3 1.8 3.3.9 4.1zm-9.1-9.1c-.9.6-2.6 0-3.7-1.5s-1.1-3.2 0-3.9c1.1-.9 2.8-.2 3.7 1.3 1.1 1.5 1.1 3.3 0 4.1zm-6.5-9.7c-.9.9-2.4.4-3.5-.6-1.1-1.3-1.3-2.8-.4-3.5.9-.9 2.4-.4 3.5.6 1.1 1.3 1.3 2.8.4 3.5zm-6.7-7.4c-.4.9-1.7 1.1-2.8.4-1.3-.6-1.9-1.7-1.5-2.6.4-.6 1.5-.9 2.8-.4 1.3.7 1.9 1.8 1.5 2.6z"></path></svg></a>
+                        {socials.map((social, i) => {
+                            const { path, label, icon } = social;
+                            return (
+                                <a key={`socail${i}`} href={path} aria-label={label} target="_blank" rel="noopener" className="nav-link">
+                                    {icon}
+                                </a>
+                            )
+                        })}
                     </div>
                 </div>
             </footer>
