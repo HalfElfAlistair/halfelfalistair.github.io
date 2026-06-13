3675d8a74e7302ef659f5b41116080b9726ca741
HalfElfAlistair <alistairjss@gmail.com>
2026-06-13 14:33:10 +0100

    adding vertical nav to dropdown when using smaller screen widths

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,32 +1,19 @@
 import { Hamburger } from '../icons/Hamburger';
+import { Cross } from '../icons/Cross';
 import { useState, useEffect } from 'react';
+import { NavContent } from './NavContent';
 
 export const Nav = () => {
-    const navLinks = [
-        'Home',
-        'About',
-        'Portfolio',
-        'Posts'
-    ]
-    // const navLinks = [
-    //     {
-    //         text: "Home",
-    //         // path: "/"
-    //     },
-    //     {
-    //         text: "About",
-    //         // path: "/about"
-    //     },
-    //     {
-    //         text: "Portfolio",
-    //         // path: "/portfolio"
-    //     },
-    //     {
-    //         text: "Posts",
-    //         // path: "/Posts"
-    //     },
-    // ]
-    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+    const [NavDropdownOpen, setNavDropdownOpen] = useState(false);
+
+    const toggleNavDropdown = () => {
+        setNavDropdownOpen(!NavDropdownOpen);
+    }
+
+    const closeNav = () => {
+        setNavDropdownOpen(false);
+    }
+
     const [screenWidth, setScreenWidth] = useState(window.innerWidth);
     const handleResize = () => {
         setScreenWidth(window.innerWidth);
@@ -54,46 +41,41 @@ export const Nav = () => {
     }, []);
 
     return (
-        <nav className={`${displayTextLinks ? "nav-with-links" : "nav-with-burger"} ${opaqueNav ? "opaqueNav" : "transparentNav"}`}>
-            {
-                displayTextLinks ? (
-                    navLinks.map(text => {
-                        return (
-                            <a key={text} href={text === "Home" ? "/" : `/${text.toLowerCase()}`} className="nav-link">{text}</a>
+        <>
+            <nav className={`${displayTextLinks ? "nav-with-links" : "nav-with-burger"} ${opaqueNav ? "opaqueNav" : "transparentNav"}`}>
+                <div className="">
+                    {
+                        displayTextLinks ? (
+                            <NavContent
+                                horizontal={true}
+                                opaqueNav={opaqueNav}
+                                closeNav={closeNav}
+                            />
+                        ) : (
+                            <button
+                                className="btn-transparent hamburger-container"
+                                onClick={() => toggleNavDropdown()}
+                            >
+                                {NavDropdownOpen ? (
+                                    <Cross fillColour={opaqueNav ? "#004C54" : "#F8F8FF"} />
+                                ) : (
+                                    <Hamburger fillColour={opaqueNav ? "#004C54" : "#F8F8FF"} />
+                                )}
+
+                            </button>
                         )
-                    })
-                ) : (
-                    <button
-                        className="btn-transparent hamburger-container"
-                        onClick={() => console.log('toggle nav menu')}
-                    >
-                        <Hamburger fillColour={opaqueNav ? "#004C54" : "#F8F8FF"} />
-                    </button>
-                )
-            }
-        </nav>
-
-    )
-
-    return (
-        <nav className={hamburgerOpen ? 'navVertical' : 'navHorizontal'} id="menu">
-
-            {screenWidth < 400 && (
-                <div className='hamContainer'>
-                    <button className='btnTransparent hamButton' onClick={() => setHamburgerOpen(!hamburgerOpen)}>
-                        <Hamburger fillColour={hamburgerOpen ? '#F1F5F2' : '#C0C0C0'} />
-                    </button>
+                    }
                 </div>
-
-            )}
-
-            {((screenWidth >= 400) || hamburgerOpen) && (
-                <div className={screenWidth < 400 ? 'verticalLinks' : 'horizontalLinks'}>
-                    {navLinks.map(text => {
-                        return <a key={text} href={`#${text.toLocaleLowerCase()}`} className={screenWidth < 400 && 'verticalLink'}>{text}</a>
-                    })}
+            </nav>
+            {NavDropdownOpen && (
+                <div className={`nav-dropdown ${opaqueNav ? "nav-dropdown-white" : "nav-dropdown-green"}`}>
+                    <NavContent
+                        horizontal={false}
+                        opaqueNav={opaqueNav}
+                        closeNav={closeNav}
+                    />
                 </div>
             )}
-        </nav>
+        </>
     )
 }
