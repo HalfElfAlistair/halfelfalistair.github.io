283969992532c352b58ad08dcb31c0db70f590a5
HalfElfAlistair <alistairjss@gmail.com>
2026-06-17 11:42:36 +0100

    creating error page and using it when article path not found

---
+++ src/pages/Error.jsx
@@ -0,0 +1,7 @@
+export const Error = ({ message }) => {
+    return (
+        <main className="error-page flex-center">
+            <h1>{message}</h1>
+        </main>
+    )
+}
--- src/routes/posts/$postId.jsx
+++ src/routes/posts/$postId.jsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { WebsiteRedesignWithFigma } from '../../pages/posts/WebsiteRedesignWithFigma';
+import { Error } from '../../pages/Error';
 
 export const Route = createFileRoute('/posts/$postId')({
   component: RouteComponent,
@@ -13,5 +14,5 @@ function RouteComponent() {
   const { postId } = Route.useParams();
   const convertedPostId = postId.toLowerCase();
   const articlepage = Articles[convertedPostId];
-  return articlepage ? articlepage : <h1 style={{ paddingTop: "200px", color: "white" }}>Page not found</h1>;
+  return articlepage ? articlepage : <Error message="Page not found" />;
 }
