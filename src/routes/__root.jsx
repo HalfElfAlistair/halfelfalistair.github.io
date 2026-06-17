a57e65a3eef586bc11d6942a68975e23c4deea86
HalfElfAlistair <alistairjss@gmail.com>
2026-06-17 11:54:10 +0100

    adding error page to routing root

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -2,9 +2,13 @@ import * as React from 'react';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { Nav } from '../components/Nav';
 import { Footer } from '../components/Footer';
+import { Error } from '../pages/Error';
 
 export const Route = createRootRoute({
     component: RootComponent,
+    notFoundComponent: () => {
+        return <Error message="Page not found" />
+    }
 })
 
 function RootComponent() {
